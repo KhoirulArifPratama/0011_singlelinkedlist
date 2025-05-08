66fc4084bed3503e6a0c23cf86e67d4f63159b4f
KhoirulArifPratama <khoirularif932@gmail.com>
2025-05-08 20:08:27 +0700

    commit 3

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -19,3 +19,20 @@ public:
     {
         START = NULL;
     }
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+        nodeBaru->next = NULL;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi NIM tidak diijinkan\n";
+                return;
+            }
