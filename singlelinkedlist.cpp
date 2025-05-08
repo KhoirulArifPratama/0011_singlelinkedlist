48fcc0cdd59a3e966cbde7489d4472fc1bf469be
KhoirulArifPratama <khoirularif932@gmail.com>
2025-05-08 20:13:48 +0700

    commit 9

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -119,3 +119,51 @@ public:
         }
     }
 };
+
+int main()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+
+    do
+    {
+        cout << endl;
+        cout << "Menu" << endl;
+        cout << "1. Menambahkan data ke dalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data dalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "Masukkan pilihan (1-5): ";
+        cin >> ch;
+
+        switch (ch)
+        {
+        case '1':
+            mhs.addNode();
+            break;
+        case '2':
+            if (mhs.listEmpty())
+            {
+                cout << "\nList Kosong" << endl;
+                break;
+            }
+
+            cout << "\nMasukkan NIM mahasiswa yang akan dihapus : ";
+            cin >> nim;
+
+            if (mhs.deleteNode(nim) == false)
+                cout << "Data tidak ditemukan" << endl;
+            else
+                cout << "Data dengan NIM " << nim << " berhasil dihapus " << endl;
+            break;
+        case '3':
+            mhs.traverse();
+            break;
+        case '4':
+            if (mhs.listEmpty() == true)
+            {
+                cout << "\nList Kosong\n";
+                break;
+            }
